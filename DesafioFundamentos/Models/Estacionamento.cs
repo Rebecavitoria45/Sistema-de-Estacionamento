03860f014e93e5318a194d34ef8515b12c4747b5
Rebecavitoria45 <rebecanascimentovit@gmail.com>
2023-12-10 11:36:03 -0300

    resolvido

---
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -0,0 +1,67 @@
+using Microsoft.Win32.SafeHandles;
+
+namespace DesafioFundamentos.Models
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            
+            
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placas = Console.ReadLine();
+              veiculos.Add(placas);
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover:");
+
+            string placa = Console.ReadLine();
+
+            // Verifica se o veículo existe
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                
+                int horas = int.Parse(Console.ReadLine());;
+                decimal valorTotal = precoInicial+(precoPorHora*horas); 
+
+                veiculos.Remove(placa);
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            // Verifica se há veículos no estacionamento
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+                
+                // *IMPLEMENTE AQUI*
+                foreach(string veiculo in veiculos ){
+                    Console.WriteLine(veiculo);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+    }
+}
