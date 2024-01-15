ac05beda42e2eeeae90e528018bcd44aadd96efb
Rebecavitoria45 <rebecanascimentovit@gmail.com>
2024-01-15 20:58:24 -0300

    ajustes

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -9,45 +9,47 @@ namespace DesafioFundamentos.Models
 {
     public class Estacionamento
     {
-        private decimal TaxaEstacionamento = 0;
-        private List<string> veiculos = new List<string>();
-        private int quantidadevagas = 3;
-        private  DateTime horaSaida;
-        private  DateTime horaEntrada;
-        private TimeSpan tempoEstacionado;
+       private decimal TaxaEstacionamento = 0;
+        private List<string> Veiculos = new List<string>();
+        private int QuantidaDeVagas = 3;
+        private  DateTime HoraSaida;
+        private  DateTime HoraEntrada;
+        private TimeSpan TempoEstacionado;
         
         public Estacionamento(decimal TaxaEstacionamento)
         {
+          if(TaxaEstacionamento<0)
+          {
+            throw new Exception("O valor da taxa não pode ser negativo");
+          }
             this.TaxaEstacionamento = TaxaEstacionamento;
         }
          public void AdicionarVeiculo()
         {
-            //verifica se existe vaga para estacionar 
-           if(quantidadevagas > 0)
+          //verifica se existe vaga para estacionar 
+           if(QuantidaDeVagas > 0)
            {
             Console.WriteLine("Digite a placa do veículo para estacionar:");
             string placa = Console.ReadLine().ToUpper();
           
-           //verifica se o carro já esta cadastrado
-           if(veiculos.Contains(placa)){ 
+          //verifica se o carro já esta cadastrado
+           if(Veiculos.Contains(placa))
+           { 
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Placa já esta cadastrada no sistema.");
                 Console.ResetColor();
                 AdicionarVeiculo();
             }
-           
             //validação da placa
              else if(Validarplaca(placa))
            {
-             veiculos.Add(placa);
-             horaEntrada = DateTime.Now;
-             quantidadevagas--;
-        
+             Veiculos.Add(placa);
+             HoraEntrada = DateTime.Now;
+             QuantidaDeVagas--;
              Console.ForegroundColor = ConsoleColor.Green;
              Console.WriteLine("Carro cadastrado com sucesso!");
              Console.ResetColor();
-
-             Console.WriteLine($"Há {quantidadevagas} vagas disponiveis no estacionamento.");
+             Console.WriteLine($"Há {QuantidaDeVagas} vagas disponiveis no estacionamento.");
             }
              //placa fora do padrão
              else 
@@ -58,11 +60,10 @@ namespace DesafioFundamentos.Models
                 AdicionarVeiculo(); //tente novamente
                } 
           }
-          
-          //sem quantidade de vaga no estacionamento
+           //sem quantidade de vaga no estacionamento
            else
          {
-              Console.ForegroundColor = ConsoleColor.Red;
+             Console.ForegroundColor = ConsoleColor.Red;
              Console.WriteLine("Não há mais vagas disponiveis no estacionamento.");
              Console.ResetColor();
             }
@@ -71,28 +72,25 @@ namespace DesafioFundamentos.Models
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
             string placa = Console.ReadLine().ToUpper();
-            
-        // Verifica se o veículo existe
-            if (veiculos.Contains(placa))
+           // Verifica se o veículo existe
+            if (Veiculos.Contains(placa))
             {
-                veiculos.Remove(placa);
-                horaSaida = DateTime.Now;
-                tempoEstacionado = horaSaida - horaEntrada;
-               
+                Veiculos.Remove(placa);
+                HoraSaida = DateTime.Now;
+                TempoEstacionado = HoraSaida - HoraEntrada;
                
                Console.Clear();
                Console.WriteLine("--------Nota Fiscal---------"); 
                Console.WriteLine("Veículo:             " +placa);
-               Console.WriteLine("Tempo estacionado:   " + FormatarTempo(tempoEstacionado));
-               Console.WriteLine("Valor total R$:      " + Calculopreco(tempoEstacionado));
+               Console.WriteLine("Tempo estacionado:   " + FormatarTempo(TempoEstacionado));
+               Console.WriteLine("Valor total R$:      " + Calculopreco(TempoEstacionado));
                    Console.WriteLine("-----------------"); 
                   
                  Console.ForegroundColor = ConsoleColor.Green;
                  Console.WriteLine("Veículo Removido com Sucesso");
                  Console.ResetColor();
-                 quantidadevagas++;
-             
-             Console.WriteLine($"Há {quantidadevagas} vagas disponiveis no estacionamento.\n");
+                 QuantidaDeVagas++;
+               Console.WriteLine($"Há {QuantidaDeVagas} vagas disponiveis no estacionamento.\n");
              }
              // veículo não cadastrado
             else
@@ -100,17 +98,16 @@ namespace DesafioFundamentos.Models
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
                 Console.ResetColor();
-               }
+             }
         }
         public void ListarVeiculos()
         {
-            // Verifica se há veículos no estacionamento
-            if (veiculos.Any())
+          // Verifica se há veículos no estacionamento
+            if (Veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
                 Console.ForegroundColor = ConsoleColor.Yellow;
-               
-                foreach(string veiculo in veiculos )
+                foreach(string veiculo in Veiculos )
                 {
                     Console.WriteLine(veiculo);
                 }
@@ -124,24 +121,25 @@ namespace DesafioFundamentos.Models
             }
         }
         //validação de placa usando expressões regulares
-       public bool Validarplaca(string placa)
+      private bool Validarplaca(string placa)
        {
           string padraoPlaca = "^[A-Z]{3}[A-Z-0-9]{4}$";
           bool validar = Regex.IsMatch(placa, padraoPlaca);
           return validar;
         }
-      decimal Calculopreco(TimeSpan tempoEstacionado)
+     private decimal Calculopreco(TimeSpan tempoEstacionado)
       {
-        if(tempoEstacionado.Minutes <= 10){
+        if(tempoEstacionado.Minutes <= 10)
+      {
         return TaxaEstacionamento;
       }
        else
        {
-       int precoHora = 5;
-       return Math.Round(TaxaEstacionamento + (tempoEstacionado.Minutes/60) * precoHora, 2); 
+      const int precoHora = 5;
+       return (tempoEstacionado.Seconds/3600)*precoHora + TaxaEstacionamento; 
       }
       }
-      private string FormatarTempo (TimeSpan tempoEstacionado)
+       private string FormatarTempo (TimeSpan tempoEstacionado)
         {
             return $"{tempoEstacionado.Hours:D2}:{tempoEstacionado.Minutes:D2}:{tempoEstacionado.Seconds:D2}";
         }
